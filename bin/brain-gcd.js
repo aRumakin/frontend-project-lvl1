9a703cbce77a25704f4e9a59dcd9967ab4e1c445
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-10-11 21:42:36 +0400

    Add brain-gcd.js

---
+++ bin/brain-gcd.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { helloName, greatestDivisor } from '../src/index.js';
+
+helloName();
+
+console.log('Find the greatest common divisor of given numbers.');
+
+greatestDivisor();
