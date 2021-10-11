7dd76556b098036574feb9d089f5714737413aec
Andrey Rumakin <rumakin.andrey@yandex.ru>
2021-10-11 21:43:17 +0400

    Fix Makefile, package.json, index.js

--- src/index.js
+++ src/index.js
@@ -49,3 +49,45 @@ export function expression() {
     }
   } return congrats();
 }
+
+export function greatestDivisor() {
+  for (let i = 1; i <= 3; i += 1) {
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+    console.log(`Question: ${num1} ${num2}`);
+    let result = 0;
+    const coll1 = [];
+    const coll2 = [];
+    for (let j = 1; j <= num1; j += 1) {
+      if (num1 % j === 0) {
+        coll1.push(j);
+      }
+    }
+    for (let k = 1; k <= num2; k += 1) {
+      if (num2 % k === 0) {
+        coll2.push(k);
+      }
+    }
+    for (let n = coll1.length - 1; n >= 0;) {
+      for (let m = coll2.length - 1; m >= 0;) {
+        if (coll1[n] < coll2[m]) {
+          m -= 1;
+        } else if (coll1[n] > coll2[m]) {
+          n -= 1;
+        } else if (coll1[n] === coll2[m]) {
+          result = coll1[n];
+          n = -1;
+          m = -1;
+        }
+      }
+    }
+    const divAnswer = readlineSync.question('Your answer: ');
+
+    if (parseInt(divAnswer, 10) === result) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${divAnswer}' is wrong answer ;(. Correct answer was '${result}'.`);
+      return console.log(`Let's try again, ${name}!`);
+    }
+  } return congrats();
+}
