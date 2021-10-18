b6d6de6251592480bc57323f85f4256f59597bc7
Andrey Rumakin <rumakin.andrey@gmail.com>
2021-10-18 09:57:55 +0400

    Fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,17 +3,21 @@ import getRandomNumber from '../mathematics.js';
 
 const gameQuestion = 'What number is missing in the progression?';
 
+function getProgression(lengthProgression, firstNumber, delta) {
+  const coll = [];
+  for (let h = 1; h <= lengthProgression; h += 1) {
+    coll.push(firstNumber + (delta * h));
+  }
+  return coll;
+}
+
 function gameQuestionAnswer() {
   const lengthProgression = getRandomNumber(5, 10);
   const delta = getRandomNumber(3, 10);
   const firstNumber = getRandomNumber(0, 100);
   const shadowNumber = getRandomNumber(0, lengthProgression);
-  let rightAnswer = '';
-  const progression = [];
-  for (let h = 1; h <= lengthProgression; h += 1) {
-    progression.push(firstNumber + (delta * h));
-  }
-  rightAnswer = progression[shadowNumber];
+  const progression = getProgression(lengthProgression, firstNumber, delta);
+  const rightAnswer = progression[shadowNumber];
   progression[shadowNumber] = '..';
 
   const question = String(progression.join(' '));
